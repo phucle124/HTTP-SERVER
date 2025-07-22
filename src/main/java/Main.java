9947fd7435ef7e4ff7e8d1d89b6665c87ef8b309
phucle124 <phuc95721@gmail.com>
2025-07-22 18:33:20 +0700

    [Initialize 1]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,12 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        System.out.println("Hello, This is my first HTTP Server in Java");
+    }
+    try{
+        ServerSocket serverSocket = new ServerSocket(60001);
 
-    // Uncomment this block to pass the first stage
-    //
-    // try {
-    //   ServerSocket serverSocket = new ServerSocket(4221);
-    //
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //
-    //   serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
-  }
+        serverSocket.setReuseAddress(true);
+    }catch(IOException e){
+        System.out.println("Error: " + e.getMessage());
+    }
 }
