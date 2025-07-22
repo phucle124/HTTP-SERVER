7deb70d76b23a68a3e91018b7628adfd2e8efaf6
phucle124 <phuc95721@gmail.com>
2025-07-22 18:52:19 +0700

    initialize

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, This is my first HTTP Server in Java");
+         try {
+      ServerSocket serverSocket = new ServerSocket(4221);
+      
+      serverSocket.setReuseAddress(true);
+      serverSocket.accept(); // Wait for connection from client.
+      System.out.println("accepted new connection");
+    } catch (IOException e) {
+      System.out.println("IOException: " + e.getMessage());
     }
-    try{
-        ServerSocket serverSocket = new ServerSocket(4221);
-
-        serverSocket.setReuseAddress(true);
-    }catch(IOException e){
-        System.out.println("Error: " + e.getMessage());
+  }
     }
+    
 }
